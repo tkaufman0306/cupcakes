88f4bcc4ec965905d7b28a7fc1c6fb5693b1926b
Taylor <taylorkkaufman@gmail.com>
2024-04-03 16:29:44 -0600

    mods

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
 
+app.app_context().push()
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "oh-so-secret"
